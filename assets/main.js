b44aebc70fd5cd18f75fbcd5e8cbcadb2e59cb03
SP ♡ <97465756+SPBarze@users.noreply.github.com>
2022-03-10 21:47:44 -0600

    Add files via upload

---
+++ assets/main.js
@@ -0,0 +1,49 @@
+// 1. Write a function that returns the sum of any two integers passed as arguments
+function addTwoNumbers(x,y) {
+  let total = x + y;
+  console.log(total);
+  return total;
+};
+
+addTwoNumbers (3, 4);
+// 2. Write a function that returns the difference of the second argument from the first
+function subtractTwowNumbers(x,y) {
+  let total = x - y;
+  console.log(total);
+  return total;
+};
+
+subtractTwowNumbers(7,9);
+// 3. Write a function that allows a user to enter their name as an
+// argument and returns a statement greeting them like, `Hello, Homer!`
+
+function greetUser (firstName) {
+const greeting = `Greetings, ${firstName}!`;
+console.log(greeting);
+return greeting;
+}
+greetUser(`SP`);
+
+
+
+// 4. Write a function that returns the product of any argument
+// multiplied by itself.
+function multipliedTwoNumbers(x,y) {
+  let total = x*y
+  console.log(total);
+
+  return total;
+};
+
+multipliedTwoNumbers(3,2);
+
+// 5. Write a  function set equal to a variable called `volume`
+function volume(l*w*h) {
+
+console.log(3, 2, 5)
+}
+
+// that returns a value of the dimensions of a box (l*w*h) and THEN
+// invoke that function in a console.log() with a message of the box's
+// dimensions
+// ^ 🔥 This is s-tier/big-brain work and not for the faint of heart 🔥
